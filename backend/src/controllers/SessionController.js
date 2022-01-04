b0c2bf586e733ed1323481a6ca918767ec64acc3
Pedro José NPS <pedrojosepaiva@outlook.com>
2022-01-03 21:42:29 -0300

    mvp do backend
    mvp do backend

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+
+module.exports = {
+  async store(req, res){
+    const { email } = req.body;
+
+    let user = await User.findOne({ email});
+
+    if(!user)
+      user = await User.create({ email });
+    else
+      return res.json({error: "Email Já Cadastrado! ", user: user})
+  
+    return res.json(user);
+  }
+}
